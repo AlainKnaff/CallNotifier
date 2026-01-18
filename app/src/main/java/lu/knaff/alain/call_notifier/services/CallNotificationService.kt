262029b0a59de1353b96e3434bf919a5be53e4c4
Alain Knaff <alain@knaff.lu>
2026-01-18 23:14:29 +0100

    "Suspicious indentation"

--- app/src/main/java/lu/knaff/alain/call_notifier/services/CallNotificationService.kt
+++ app/src/main/java/lu/knaff/alain/call_notifier/services/CallNotificationService.kt
@@ -13,18 +13,18 @@ public class CallNotificationService : CallScreeningService() {
     private final val TAG="CallScreeningService"
 
     override fun onScreenCall (callDetails: Call.Details) {
-	Log.i(TAG, "Call notified");
+        Log.i(TAG, "Call notified");
 
-	if(callDetails.getCallDirection() == Call.Details.DIRECTION_INCOMING) {
+        if(callDetails.getCallDirection() == Call.Details.DIRECTION_INCOMING) {
             var number = "unknown"
-	    val handle = callDetails.handle
-	    if(handle != null) {
+            val handle = callDetails.handle
+            if(handle != null) {
                 number = handle.schemeSpecificPart
-	        Log.i(TAG, "Phone number: "+number)
-	    }
+                Log.i(TAG, "Phone number: "+number)
+            }
             respondToCall(callDetails, CallResponse.Builder().build())
             val msg = "Call from "+number+" on "+Date();
             SendMail.send(this, msg, msg, null)
-	}
+        }
     }
 }
