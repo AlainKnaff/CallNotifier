07351cb101346135c4388c6ffc0be0c0b42c83b3
Alain Knaff <alain@knaff.lu>
2026-01-18 23:12:34 +0100

    Actually send mail if call received

--- app/src/main/java/lu/knaff/alain/call_notifier/services/CallNotificationService.kt
+++ app/src/main/java/lu/knaff/alain/call_notifier/services/CallNotificationService.kt
@@ -1,22 +1,30 @@
 package lu.knaff.alain.call_notifier.services
 
+import java.util.Date
+
 import android.util.Log
 
 import android.telecom.Call
 import android.telecom.CallScreeningService
 
+import lu.knaff.alain.call_notifier.SendMail
+
 public class CallNotificationService : CallScreeningService() {
     private final val TAG="CallScreeningService"
 
     override fun onScreenCall (callDetails: Call.Details) {
-	Log.i(TAG, "call detected");
+	Log.i(TAG, "Call notified");
 
 	if(callDetails.getCallDirection() == Call.Details.DIRECTION_INCOMING) {
-	    Log.i(TAG, "Call from display name: "+callDetails.getCallerDisplayName())
+            var number = "unknown"
 	    val handle = callDetails.handle
-	    if(handle != null)
-		Log.i(TAG, "Handle: "+handle.schemeSpecificPart)
-	    respondToCall(callDetails, CallResponse.Builder().build())
+	    if(handle != null) {
+                number = handle.schemeSpecificPart
+	        Log.i(TAG, "Phone number: "+number)
+	    }
+            respondToCall(callDetails, CallResponse.Builder().build())
+            val msg = "Call from "+number+" on "+Date();
+            SendMail.send(this, msg, msg, null)
 	}
     }
 }
